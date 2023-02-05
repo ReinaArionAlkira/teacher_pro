ea42424c73a855fe65c0874c8c61872f9c015d15
Reina Arion Alkira <you@example.com>
2023-02-05 15:36:44 +0100

    Some validation, files sctructure

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:teacher_pro/models/grade.dart';
 import 'package:teacher_pro/models/lesson.dart';
 import 'package:teacher_pro/models/student.dart';
 
+import 'widgets/home_screen.dart';
+
 Future<void> main() async {
   runApp(const MyApp());
 }
@@ -17,18 +19,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepPurple,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomeScreen(),
+      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
---
+++ lib/widgets/add_student_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AddStudentWidget extends StatelessWidget {
+  const AddStudentWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
